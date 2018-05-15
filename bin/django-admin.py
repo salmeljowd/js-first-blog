6970ee3797c01bea209aa821ef108886039373dd
juhaswd <oskar.testaaja01@gmail.com>
2018-05-15 15:31:10 +0300

    My Django Girls app, first commit

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/mnt/c/Users/josal/ubuntuvenv/jsenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ jsblog/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class JsblogConfig(AppConfig):
+    name = 'jsblog'
